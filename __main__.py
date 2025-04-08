72ff36b798cbcd57910a613dfb5548940a5ce411
MitchellGerdisch <mitch@pulumi.com>
2025-04-08 09:17:18 -0500

    dependency

--- __main__.py
+++ __main__.py
@@ -1,6 +1,4 @@
 from pulumi.provider.experimental import component_provider_host
 
-from service import Service
-
 if __name__ == "__main__":
     component_provider_host(name="gke", components=[Cluster])
