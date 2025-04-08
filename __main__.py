e49473bb9341359f0e13a8bcdc790621ac07c530
MitchellGerdisch <mitch@pulumi.com>
2025-04-08 09:18:41 -0500

    module ref

--- __main__.py
+++ __main__.py
@@ -1,4 +1,6 @@
 from pulumi.provider.experimental import component_provider_host
 
+from cluster import Cluster
+
 if __name__ == "__main__":
     component_provider_host(name="gke", components=[Cluster])
